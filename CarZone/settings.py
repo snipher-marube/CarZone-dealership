fbf0c27f9337ff75e80c14fa5f059ed85b6e6bd0
snipher <sniphermarube@gmail.com>
2022-08-02 14:39:40 +0300

    Initial commit

---
+++ CarZone/settings.py
@@ -0,0 +1,217 @@
+from pathlib import Path
+from django.contrib.messages import constants as messages
+import dj_database_url
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-%wm_ygiu3i)n(n66a$+dtxy%84y^k%vu^vm-lh@sljl5m(97o8'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'social_django',
+    'django.contrib.humanize',
+    'pages.apps.PagesConfig',
+    'cars.apps.CarsConfig',
+    'accounts.apps.AccountsConfig',
+    'contacts.apps.ContactsConfig',
+    'ckeditor',
+    'ckeditor_uploader',
+    'multiselectfield',
+    'whitenoise',
+    'cloudinary_storage',
+    'cloudinary',
+
+    'django.contrib.sites',
+
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'CarZone.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates']
+        ,
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'CarZone.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':  'carzone',
+        'USER': 'postgres',
+        'EMAIL': 'sniphermarube@gmail.com',
+        'PASSWORD': 'snipher8431',
+        #'HOST': 'database-1.cl65vrvozval.us-east-1.rds.amazonaws.com',
+        'PORT': '5432',
+    }
+}
+
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
+
+# Password validation
+# https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/4.0/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.0/howto/static-files/
+
+STATIC_URL = 'static/'
+MEDIA_URL = 'images/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+STATIC_ROOT = BASE_DIR / 'staticfiles/'
+MEDIA_ROOT = BASE_DIR / 'static/images'
+
+CKEDITOR_UPLOAD_PATH = 'uploads/'
+
+CKEDITOR_CONFIGS = {
+    'default': {
+        'toolbar': 'full',
+        'height': 300,
+        'width': '100%',
+    },
+}
+
+# sending email setting
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'snipherblog@gmail.com'
+EMAIL_HOST_PASSWORD = 'jydqhivfgiamdcjs'
+EMAIL_USE_TLS = True
+
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': 'snipher',
+    'API_KEY': '421162441822348',
+    'API_SECRET': 'gpP7UebOBTHV99kXyBfPDtW2fmk'
+}
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+# redirect login to dashboard, instead of profiles
+LOGIN_REDIRECT_URL = 'dashboard'
+
+# DJANGO Messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+
+}
+SOCIAL_AUTH_JSONFIELD_ENABLED = True
+
+AUTHENTICATION_BACKENDS = [
+    'social_core.backends.google.GoogleOAuth2',
+    'social_core.backends.facebook.FacebookOAuth2',
+    'social_core.backends.github.GithubOAuth2',
+    'django.contrib.auth.backends.ModelBackend',
+]
+SOCIAL_AUTH_URL_NAMESPACE = 'social'
+
+
+
+SOCIAL_AUTH_FIELDS_STORED_IN_SESSION = ['state']
+SESSION_COOKIE_SECURE = False
+
+
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.social_auth.associate_by_email',
+    'social_core.pipeline.user.create_user',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+)
+
+
+SOCIAL_AUTH_ADMIN_USER_SEARCH_FIELDS = ['username', 'first_name', 'last_name', 'email']
+
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '1070849170673-53e62tkh0mdm07pmsd1jv4u4cdb5n1t8.apps.googleusercontent.com'
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = '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'
+
+SOCIAL_AUTH_GITHUB_KEY = '4b7bec5cff3c540dc18f'
+SOCIAL_AUTH_GITHUB_SECRET = 'd176841b614ac598be9dd1118a36fb2d3eb1d7d6'
+# rel = social login Facebook and Google
+SITE_ID = 1
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
---
+++ cars/models.py
@@ -0,0 +1,118 @@
+from distutils.command.upload import upload
+from django.db import models
+from datetime import datetime
+from ckeditor_uploader.fields import RichTextUploadingField
+from multiselectfield import MultiSelectField
+
+
+class Car(models.Model):
+    STATE_CHOICES = (
+        ('MU', 'Meru'),
+        ('MA', 'Mombasa'),
+        ('KU', 'Kisumu'),
+        ('KI', 'Kisii'),
+        ('MS', 'Machakos'),
+        ('NI', 'Nairobi'),
+        ('GA', 'Garissa'),
+        ('NA', 'Nyamira'),
+        ('KO', 'Kericho'),
+        ('EM', 'Embu'),
+        ('NV', 'Naivasha'),
+        ('NK', 'Nakuru'),
+        ('NM', 'Namanga'),
+        ('MI', 'Migori'),
+        ('KG', 'Kakamega'),
+        ('EL', 'Eldoret'),
+        ('BM', 'Bungoma'),
+        ('KL', 'Kwale'),
+        ('LM', 'Limuru'),
+        ('KR', 'Kilgoris'),
+        ('KT', 'Kitengela'),
+        ('KN', 'Karen'),
+        ('LV', 'Lavington'),
+        ('NR', 'Nyeri'),
+        ('MA', 'Mwea'),
+        ('HB', 'Homa Bay'),
+        ('OG', 'Ogembo'),
+        ('SY', 'Siaya'),
+        ('KF', 'Kilifi'),
+        ('MD', 'Malindi'),
+        ('NG', 'Namanga'),
+        ('NM', 'New Mexico'),
+
+    )
+
+    YEAR_CHOICE = []
+    for year in range(2000, (datetime.now().year+1)):
+        YEAR_CHOICE.append((year, year))
+
+    FEATURES_CHOICES = (
+        ('Cruise Control', 'Cruise Control'),
+        ('Audio Interface', 'Audio Interface'),
+        ('Airbags', 'Airbags'),
+        ('Air Conditioning', 'Air Conditioning'),
+        ('Seat Heating', 'Seat Heating'),
+        ('Alarm System', 'Alarm System'),
+        ('ParkAssist', 'ParkAssist'),
+        ('Power Steering', 'Power Steering'),
+        ('Reversing Camera', 'Reversing Camera'),
+        ('Direct Fuel Injection', 'Direct Fuel Injection'),
+        ('Auto Start/Stop', 'Auto Start/Stop'),
+        ('Wind Deflector', 'Wind Deflector'),
+        ('Bluetooth Handset', 'Bluetooth Handset'),
+    )
+
+    DOOR_CHOICES = (
+        ('2', '2'),
+        ('3', '3'),
+        ('4', '4'),
+        ('5', '5'),
+        ('6', '6'),
+    )
+
+    TRANSMISSION_MODES = (
+        ('AT', 'Automatic'),
+        ('MT', 'Manual'),
+        ('AM', 'Automated Manual'),
+        ('CVT', 'Continuosly Variable'),
+    )
+    CAR_CONDITION = (
+        ('u', 'Used'),
+        ('n', 'New'),
+        ('r', 'Refurbished'),
+    )
+
+    car_title = models.CharField(max_length=100)
+    state = models.CharField(choices=STATE_CHOICES, max_length=2)
+    color = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    year = models.IntegerField(('year'), choices=YEAR_CHOICE)
+    condition = models.CharField(choices=CAR_CONDITION, max_length=1)
+    price = models.IntegerField()
+    description = RichTextUploadingField()
+    car_photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    car_photo_1 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    car_photo_2 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    car_photo_3 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    car_photo_4 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    features = MultiSelectField(choices=FEATURES_CHOICES)
+    body_style = models.CharField(max_length=100)
+    engine = models.CharField(max_length=100)
+    transmission = models.CharField(choices=TRANSMISSION_MODES, max_length=3)
+    interior = models.CharField(max_length=100)
+    miles = models.IntegerField()
+    doors = models.CharField(choices=DOOR_CHOICES, max_length=10)
+    passengers = models.IntegerField()
+    vin_no = models.CharField(max_length=100)
+    milage = models.IntegerField()
+    fuel_type = models.CharField(max_length=50)
+    no_of_owners = models.CharField(max_length=100)
+    is_featured = models.BooleanField(default=False)
+    created_date = models.DateTimeField(default=datetime.now, blank=True)
+
+    class Meta:
+        ordering = ['-created_date']  # order by latest
+
+    def __str__(self):
+        return self.car_title
