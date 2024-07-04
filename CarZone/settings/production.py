eb821d40507c581133d8cfcfa19ef04a311c1e34
snipher <sniphermarube@gmail.com>
2024-07-04 14:23:56 +0300

    vecel deployment

--- CarZone/settings/production.py
+++ CarZone/settings/production.py
@@ -1,25 +1,21 @@
 from .base import *
+from decouple import config, Csv
+import dj_database_url
 
 # Production-specific settings
 DEBUG = False
 
 # Add your production domain to ALLOWED_HOSTS
-ALLOWED_HOSTS = [
-    'carzonemeru.herokuapp.com',
-]
+ALLOWED_HOSTS = ['.vercel.app']
 
 # Configure your production database (example using PostgreSQL)
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT', default=''),
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
-
 # Static and media files settings for production
 STATIC_URL = '/static/'
 
@@ -35,3 +31,14 @@ EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
 # Cloudinary storage for production
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+SECURE_HSTS_SECONDS = 60
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_HSTS_PRELOAD = True
