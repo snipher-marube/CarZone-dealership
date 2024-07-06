48e816dde3bcaae827b08c4491bc9c98f15e3ee1
snipher <sniphermarube@gmail.com>
2024-07-06 21:10:03 +0300

    distutils

--- build.sh
+++ build.sh
@@ -3,7 +3,10 @@
 # Build the project
 echo "Building the project..."
 python3.9 -m pip install --upgrade pip
+pip3 install distutils
 pip3 install -r requirements.txt
+pip3 install setuptools
+pip3 freeze > requirements.txt
 
 echo "make migrations..."
 python3.9 manage.py makemigrations
@@ -14,5 +17,3 @@ python3.9 manage.py collectstatic --noinput --clear
 
 echo "create default superuser..."
 python3.9 manage.py create_default_superuser
-
-
