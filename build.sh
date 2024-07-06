dfcaff26c40c1f02c51b77f4be92b03009e26545
snipher <sniphermarube@gmail.com>
2024-07-06 21:43:13 +0300

    distutils

--- build.sh
+++ build.sh
@@ -4,13 +4,21 @@
 echo "Building the project..."
 python3.12 -m pip install --upgrade pip
 
+# Create a temporary requirements file for distutils and setuptools
+echo "distutils" > temp-requirements.txt
+echo "setuptools" >> temp-requirements.txt
+
+# Install distutils and setuptools separately
+pip3 install -r temp-requirements.txt
+
+# Remove temporary requirements file
+rm temp-requirements.txt
+
 # Uninstall all packages
 pip freeze | xargs pip uninstall -y
 
 # Install required packages
-pip install distutils
 pip install -r requirements.txt
-pip install setuptools
 
 echo "make migrations..."
 python3.12 manage.py makemigrations
