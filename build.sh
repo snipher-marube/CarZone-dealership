2c57570ccc0bbfb4046f767e26caf783c18a2562
snipher <sniphermarube@gmail.com>
2024-07-06 18:17:59 +0300

    build up

--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@ python3.12 -m pip install --upgrade pip
 pip3 install -r requirements.txt
 
 echo "make migrations..."
-python3.12 manage.py makemigrations --noinput
-python3.12 manage.py migrate --noinput
+python3.12 manage.py makemigrations
+python3.12 manage.py migrate 
 
 echo "collect static..."
 python3.12 manage.py collectstatic --noinput --clear
