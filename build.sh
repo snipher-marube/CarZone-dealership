28e5b8e62d57ce42757e0253a09e5486dcf647a6
snipher <sniphermarube@gmail.com>
2024-07-06 20:22:08 +0300

    new

--- build.sh
+++ build.sh
@@ -2,17 +2,17 @@
 
 # Build the project
 echo "Building the project..."
-python3.12 -m pip install --upgrade pip
+python3.9 -m pip install --upgrade pip
 pip3 install -r requirements.txt
 
 echo "make migrations..."
-python3.12 manage.py makemigrations
-python3.12 manage.py migrate 
+python3.9 manage.py makemigrations
+python3.9 manage.py migrate 
 
 echo "collect static..."
-python3.12 manage.py collectstatic --noinput --clear
+python3.9 manage.py collectstatic --noinput --clear
 
 echo "create default superuser..."
-python3.12 manage.py create_default_superuser
+python3.9 manage.py create_default_superuser
 
 
