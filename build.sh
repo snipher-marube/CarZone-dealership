4d202466ec4ab9ccdb2cef2fc2c5559b4d84c025
snipher <sniphermarube@gmail.com>
2024-07-06 17:46:38 +0300

    build up

--- build.sh
+++ build.sh
@@ -2,13 +2,14 @@
 
 # Build the project
 echo "Building the project..."
-pip install -r requirements.txt
+python3.12 -m pip install --upgrade pip
+pip3 install -r requirements.txt
 
 echo "make migrations..."
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
+python3.12 manage.py makemigrations --noinput
+python3.12 manage.py migrate --noinput
 
 echo "collect static..."
-python manage.py collectstatic --noinput --clear
+python3.12 manage.py collectstatic --noinput --clear
 
 
