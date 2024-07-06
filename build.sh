128c022dca19c615581c754f4155374a77a39771
snipher <sniphermarube@gmail.com>
2024-07-06 21:30:30 +0300

    distutils

--- build.sh
+++ build.sh
@@ -2,18 +2,22 @@
 
 # Build the project
 echo "Building the project..."
-python3.9 -m pip install --upgrade pip
-pip3 install distutils
-pip3 install -r requirements.txt
-pip3 install setuptools
-pip3 freeze > requirements.txt
+python3.12 -m pip install --upgrade pip
+
+# Uninstall all packages
+pip freeze | xargs pip uninstall -y
+
+# Install required packages
+pip install distutils
+pip install -r requirements.txt
+pip install setuptools
 
 echo "make migrations..."
-python3.9 manage.py makemigrations
-python3.9 manage.py migrate 
+python3.12 manage.py makemigrations
+python3.12 manage.py migrate 
 
 echo "collect static..."
-python3.9 manage.py collectstatic --noinput --clear
+python3.12 manage.py collectstatic --noinput --clear
 
 echo "create default superuser..."
-python3.9 manage.py create_default_superuser
+python3.12 manage.py create_default_superuser
